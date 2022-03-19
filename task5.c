751eefc10a5092561326e4e845722ec9d9097f23
UnitedFeodor <86407851+UnitedFeodor@users.noreply.github.com>
2022-03-19 10:27:41 +0300

    Update task5.c

--- task5.c
+++ task5.c
@@ -9,25 +9,25 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	FILE* file_1 = fopen(argv[1], "r");
+	FILE* file1 = fopen(argv[1], "r");
 
-	if (file_1 == NULL) {
+	if (file1 == NULL) {
 		fprintf(stderr,"error! couldn't open the file");
 
 		return 1;
 	}
 
-	FILE* file_2 = fopen(argv[2], "w");
+	FILE* file2 = fopen(argv[2], "w");
 
-	if (file_2 == NULL){
+	if (file2 == NULL){
 		fprintf(stderr,"error! couldn't open the file");
 		return 1;
 	}
 
 
 	char symb;
-	while ((symb = fgetc(file_1)) != EOF){
-		fputc(symb, file_2);
+	while ((symb = fgetc(file1)) != EOF){
+		fputc(symb, file2);
 	}
 
 	struct stat copyStat;
@@ -35,6 +35,14 @@ int main(int argc, char *argv[]){
 
 	chmod(argv[2], copyStat.st_mode);
 
-	fclose(file_1);
-	fclose(file_2);
+
+	 if (fclose(file1) != 0) {
+        
+        fprintf(stderr,"error! could not close the file\n");
+    
+	 if (fclose(file2) != 0) {
+        
+        fprintf(stderr,"error! could not close the file\n");
+    }
+	//fclose(file2);
 }

78449f5f06f4179d402f90b5411b22b83eb89cc6
UnitedFeodor <86407851+UnitedFeodor@users.noreply.github.com>
2022-03-19 10:26:31 +0300

    Update task4.c

--- task4.c
+++ task4.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <errno.h>
 int main(int argc, char *argv[]){
     if (argc != 3){
         fprintf(stderr,"error! wrong arguments: 1 - file name, 2 - N strings in group");
         return 1;
     }
 
-    int lines = atoi(argv[2]);
-    int  groupNumber = 0;
+    //int lines = atoi(argv[2]);
+    char *test;
+    errno = 0;
+    int lines = strtol(argv[2],&test,10);
+    
+    if ((strlen(test) != 0) || (errno!=0)) {
+	    fprintf(stderr,"error! not a number or over/underflow: error %d\n",errno);
+	    return 1;
+
+    }	
+    if (test == argv[2]) {
+        fprintf(stderr, "error! empty string\n");
+        return 1;
+    }
+
+    if (lines < 0) {
+        fprintf(stderr, "error! amount less than zero\n");
+        return 1;    
+    }
+
 
     FILE* file = fopen(argv[1], "r");
+    if (file == NULL) {
+		fprintf(stderr,"error! couldn't open the file");
+
+		return 1;
+	}
     char inp;
     int currLines = 1;
 
@@ -21,12 +45,16 @@ int main(int argc, char *argv[]){
             currLines++;
             if ((lines != 0) && (currLines > lines)){
                 getc(stdin);
-                putc('\n', stdout);
+                //putc('\n', stdout);
                 currLines = 1;
             }
         }
     }
+    if (fclose(file) != 0) {
+        
+        fprintf(stderr,"error! could not close the file\n");
+        return 1;
+    }
 
-    fclose(file);
     return 0;
 }
